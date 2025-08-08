047c609c36dc22a172243d5125ae20516fce2d96
laladwesh <guptaavinash302@gmail.com>
2025-08-08 12:53:20 +0530

    new routes adde

--- src/App.js
+++ src/App.js
@@ -9,13 +9,17 @@ import Footer from './components/Footer.jsx'
 import Home from './pages/Home.jsx'
 import { Route, Routes } from 'react-router-dom'
 import VerticalPage from './pages/VerticalPage.jsx'
+import TeamPage from './pages/TeamPage.jsx'
+import CoordinatorPage from './pages/CoordinatorPage.jsx'
 const App = () => {
   return (
-    <div>
+    <div className='overflow-x-hidden'>
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/vertical" element={<VerticalPage />} />
+        <Route path="/vertical" element={<VerticalPage />} />3
+        <Route path="/team" element={<TeamPage />} />
+        <Route path='/coordinators' element={<CoordinatorPage />} />
         {/* Add more routes as needed */}
       </Routes>
       <Footer/>
