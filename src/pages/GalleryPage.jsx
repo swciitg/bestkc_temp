d2b9e080ead870c57036fdb97e8c0286de01cdb3
laladwesh <guptaavinash302@gmail.com>
2025-09-06 08:51:37 +0530

    gallary page created

---
+++ src/pages/GalleryPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ComingSoon from '../components/Coming'
+
+const GalleryPage = () => {
+  return (
+    <div>
+        <ComingSoon />
+    </div>
+  )
+}
+
+export default GalleryPage
